40e518066ab2ce0a344a639ca493468e17ae6e18
Basma <basma.bouharicha@epita.fr>
2021-07-10 18:02:46 +0200

    Codecov Test with Sample

--- src/main/java/fr/lernejo/navy_battle/Server.java
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -12,7 +12,6 @@ public class Server {
     public void createServer(int port) throws IOException {
         //int port = Integer.parseInt(System.getenv().getOrDefault("PORT", args[0]));
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-
         server.createContext("/ping", (var exchange) -> {
             String body = "Are you OK ?";
             exchange.sendResponseHeaders(200, body.length());
@@ -24,7 +23,10 @@ public class Server {
         ExecutorService executor = Executors.newFixedThreadPool(1);
         server.setExecutor(executor);
         server.start();
-
+        server.stop(20);
     }
-    public void stopServer(int port){}
+
+    public void stopServer() {
+        //server.stop();
+    }
 }
